41c7fec793fa50f8c2d07257ac8eee753f2137ef
WonKyoung <dnjsrud529@naver.com>
2021-06-03 17:51:22 +0900

    버튼 및 log 보이도록 추가

--- src/main/java/com/glowsoft/spring_test/controller/HomeController.java
+++ src/main/java/com/glowsoft/spring_test/controller/HomeController.java
@@ -15,16 +15,24 @@ import org.springframework.web.client.RestTemplate;
 
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 
 @Controller
 public class HomeController {
     private String cookie;
+    public List<String> data = new ArrayList<>();
 
     @RequestMapping(value = "/main.do", method = RequestMethod.GET)
     public String index(Model model) {
-        model.addAttribute("data", "Hello, Spring from IntelliJ! :)");
+        //model.addAttribute("data", "Hello, Spring from IntelliJ! :)");
+        data.add("");
+        //model.addAttribute("data",data);
         System.out.println("main in!");
         return "main";
     }
@@ -41,8 +49,8 @@ public class HomeController {
         return "redirect:http://naver.com";
     }
 
-    @RequestMapping(value = "/code.do")
-    public ModelAndView ModelAndView(HttpServletResponse response) {
+    @RequestMapping(value = "/login.do")
+    public ModelAndView ModelAndView(HttpServletRequest request, HttpServletResponse response) {
         ModelAndView mav = new ModelAndView("main");
 //        Map<String,Object> map = new HashMap<String,Object>();
 //        map.put("username","admin");
@@ -56,29 +64,59 @@ public class HomeController {
 //
 //        }
 
+        String name = request.getParameter("username");
+        String password = request.getParameter("password");
+        //List<String> data = new ArrayList<>();
+        //data.add(request.getParameter("data"));
+
         MultiValueMap<String, String> params = new LinkedMultiValueMap<String, String>();
-        params.add("username","test");
-        params.add("password","1234qwer!");
+        params.add("username",name);
+        params.add("password",password);
         params.add("REQUEST_KIND","API");
 
         HttpEntity entity = new HttpEntity(params,null);
-
-        RestTemplate restTemplate = new RestTemplate();
-        ResponseEntity<String> responseEntity = restTemplate.exchange("http://localhost:8080/user/login/process", HttpMethod.POST,entity,String.class);
-        cookie = responseEntity.getHeaders().get("SET-COOKIE").get(0);
-        mav.addObject("data",cookie);
-        response.addHeader("SET-COOKIE",cookie);
-        System.out.println(responseEntity.getStatusCode());
-        System.out.println(responseEntity.getBody());
-        return mav;
+        try {
+            RestTemplate restTemplate = new RestTemplate();
+            ResponseEntity<String> responseEntity = restTemplate.exchange("http://localhost:8080/user/login/process", HttpMethod.POST, entity, String.class);
+            cookie = responseEntity.getHeaders().get("SET-COOKIE").get(0);
+            data.add(responseEntity.getStatusCode() + "\n" + responseEntity.getBody()+"\n\n");
+            mav.addObject("data", data);
+            response.addHeader("SET-COOKIE", cookie);
+            System.out.println(responseEntity.getStatusCode());
+            System.out.println(responseEntity.getBody());
+            return mav;
+        } catch (Exception e){
+            data.add("fail\n\n");
+            mav.addObject("data",data);
+            return mav;
+        }
     }
 
     @RequestMapping("/anal.do")
-    public ModelAndView anal(){
-        ModelAndView mav = new ModelAndView("/index.jsp");
+    public ModelAndView anal(HttpServletRequest request){
+        ModelAndView mav = new ModelAndView("main");
+        //String data = request.getParameter("data");
 
+        try {
         HttpHeaders headers = new HttpHeaders();
         headers.add("Cookie",cookie);
+
+        String project = request.getParameter("project");
+        String filepath = request.getParameter("filepath");
+
+
+        List<String> files = new ArrayList<>();
+
+        findFile(filepath,files);
+
+        String analFiles = "";
+
+        for(int i=0;i<files.size();i++){
+            if(i!=files.size()-1)
+                analFiles += files.get(i)+",";
+            else
+                analFiles += files.get(i);
+        }
 
 //        File file = new File("C:\\Users\\dnjsr\\pro_c\\pro_c.zip");
 //
@@ -109,14 +147,35 @@ public class HomeController {
 //
 //        MultipartFile multipartFile = new CommonsMultipartFile(fileItem);
 //
-//
+//      analFiles
         MultiValueMap<String, Object> params = new LinkedMultiValueMap<String, Object>();
         params.add("projectname","test");
-        params.add("filelist","C:\\Users\\dnjsr\\pro_c\\cpdemo1\\cpdemo1.pc,C:\\Users\\dnjsr\\pro_c\\sample10\\sample10.pc");
+        params.add("filelist",analFiles);
         HttpEntity entity = new HttpEntity(params,headers);
 
-        RestTemplate restTemplate = new RestTemplate();
-        ResponseEntity<String> responseEntity = restTemplate.exchange("http://localhost:8080/api/analyzeCluster", HttpMethod.POST,entity,String.class);
-        return mav;
+            RestTemplate restTemplate = new RestTemplate();
+            ResponseEntity<String> responseEntity = restTemplate.exchange("http://localhost:8080/api/analyzeCluster", HttpMethod.POST, entity, String.class);
+            data.add(responseEntity.getStatusCode() + "\n" + responseEntity.getBody()+"\n\n");
+            mav.addObject("data",data);
+            System.out.println(responseEntity.getStatusCode());
+            System.out.println(responseEntity.getBody());
+            return mav;
+        }catch (Exception e){
+            data.add("anal fail\n\n");
+            mav.addObject("data",data);
+            return mav;
+        }
+    }
+
+    public static void findFile(String path,List<String> filelist){
+        File[] files = new File(path).listFiles();
+
+        for(File f : files){
+            if(f.isDirectory()) {
+                findFile(f.getAbsolutePath(),filelist);
+            } else {
+                filelist.add(f.getAbsolutePath());
+            }
+        }
     }
 }
--- web/WEB-INF/views/main.jsp
+++ web/WEB-INF/views/main.jsp
@@ -1,4 +1,7 @@
+<%@ page import="java.util.List" %>
+<%@ page import="java.util.ArrayList" %>
 <%--
+
   Created by IntelliJ IDEA.
   User: codemind
   Date: 2021-06-03
@@ -6,11 +9,59 @@
   To change this template use File | Settings | File Templates.
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%--<%--%>
+<%--    List<String> log = new ArrayList<>();--%>
+<%--    try {--%>
+<%--        log = (List<String>) request.getAttribute("data");--%>
+<%--        if (log == null)--%>
+<%--            log.add("");--%>
+<%--    } catch(Exception e){--%>
+<%--        log.add("");--%>
+<%--    }--%>
+<%--%>--%>
 <html>
 <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <title>Title</title>
 </head>
 <body>
-    <h3>${data}</h3>
+    <div>
+        <form action="/login.do" method="get">
+            <input type="text" name="username" placeholder="아이디" value=""/>
+            <input type="password" name="password" placeholder="비밀번호" value=""/>
+<%--            <input type="hidden" name="data" value=${data}/>--%>
+            <input type="submit" value="전송"/>
+        </form>
+    </div>
+    <div>
+        <form action="/anal.do" method="get">
+            <input type="text" name="project" placeholder="프로젝트이름" value=""/>
+            <input type="text" name="filepath" placeholder="파일경로" value=""/>
+<%--            <input type="hidden" name="data" value=${data}/>--%>
+            <input type="submit" value="전송"/>
+        </form>
+    </div>
+
+<%--<%--%>
+<%--    for(int i=0;i< log.size();i++){--%>
+<%--%>--%>
+<%--    <p><%=log.get(i)%>--%>
+<%--<%--%>
+<%--    }--%>
+<%--%>--%>
+<%--    <c:forEach var="l" items="${data}">--%>
+<%--        <h3>${l}--%>
+<%--    </c:forEach>--%>
+
+<%
+    List<String> log = (List<String>) request.getAttribute("data");
+    if(log != null)
+    for(String s : log){
+%>
+       <p><%=s%>
+<%
+    }
+%>
+
 </body>
 </html>
